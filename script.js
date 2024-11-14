a85238c886eb4a0389a8e4de59ee546edf8043e4
TGore714 <127151123+TGore714@users.noreply.github.com>
2024-11-13 21:23:13 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('bookingForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert("Thank you for reaching out! We'll get back to you soon.");
+});
